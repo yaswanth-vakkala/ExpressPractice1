4ded1fb59285a753591882b6c502c382430d870f
yaswanth-vakkala <yaswanthv87@gmail.com>
2022-09-24 23:24:36 +0530

    build app with template engine

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ const express = require("express");
 require("dotenv").config();
 const path = require("path");
 const logger = require("./middleware/logger");
+const exphbs = require("express-handlebars");
+const members = require("./Members");
 
 const app = express();
+
+//init middleware
+// app.use(logger);
+
+//usually if we have react or vue or other frontend we build api that serves json or a server that just serves template files not both
+//handlebars middleware
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 //body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//init middleware
-// app.use(logger);
+//Homepage route
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Member App",
+    members,
+  }); //showing coz first in file while static file is defined at last
+});
+
 //everytime we make a request middleware runs so we can define a functionality inside it that can be useful
 
 // app.get("/", (req, res) => {
--- routes/api/members.js
+++ routes/api/members.js
@@ -32,7 +32,8 @@ router.post("/", (req, res) => {
     return res.status(400).json({ msg: "name field missing" });
   }
   members.push(newMember);
-  res.json(members);
+  // res.json(members);
+  res.redirect("/");
 });
 
 //update member
